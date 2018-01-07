c1e74159b58b6bb5263ac6a9ff02586acb7712ff
Felix Harrison <fmlharrison@gmail.com>
2018-01-07 15:22:54 +0000

    Fixed issue with CORS middleware handler

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
+  next();
 });
 
 // Here we declear the base routes that our controllers will be called from.
