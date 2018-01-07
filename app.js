6e02bfdfcf14d189857b01b26974b9836ecf9d89
Felix Harrison <fmlharrison@gmail.com>
2018-01-07 15:20:25 +0000

    Added CORS access in server file

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // This extracts json data from requests and parsers them into the required model.
 app.use(bodyParser.json());
 
+// Added headers to the response to make sure that everything is CORS safe.
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  // An OPTIONS request is made first to see if the browser can make the request it wants to.
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+});
+
 // Here we declear the base routes that our controllers will be called from.
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
